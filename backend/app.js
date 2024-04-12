e93cb4303863a5c3e5c4e0825118b9e5e192735a
timmy.jinks <tyson.j.jenkins.github@gmail.com>
2024-04-11 21:33:30 -0600

    Add file structure

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('./libs/node_modules/express');
+const app = express();
+const port = 8080;
+
+app.listen(port);
+app.use(express.static('frontend'))
+
+app.get('/home', (req, res) => {
+    console.log("connected");
+
+    res.sendFile('index.html', {root: 'frontend'});
+});
